0b572bc0dadd406849e24d61ac30cddca4d35c8c
이지우 <20241257@sungshin.ac.kr>
2025-06-30 16:39:03 +0900

    LostItem 모델 생성, 마이그레이션 완료, admin 등록

--- backend/meetagain/apps.py
+++ backend/meetagain/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class LostConfig(AppConfig):
+class MeetagainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lost'
+    name = 'meetagain' #lost가 없길래 meetagain으로 변경함.
--- backend/meetagain/models.py
+++ backend/meetagain/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class LostItem(models.Model):
+    name = models.CharField(max_length=100)        # 물건 이름
+    description = models.TextField(blank=True)     # 물건 특징 설명 (공백 가능)
+    
+    lost_contact = models.CharField(max_length=20, blank=True) # 분실자 연락처
+    lost_location = models.CharField(max_length=200)      # 분실 위치
+    lost_date = models.DateField()                         # 분실 날짜
+    
+    found_location = models.CharField(max_length=200)     # 발견 위치
+    found_date = models.DateField()                       # 발견 날짜
+    
+    is_claimed = models.BooleanField(default=False)    # meetagain(소유자 반환) 여부
+    image = models.ImageField(upload_to='lost_items/', blank=True, null=True)  # 이미지 파일 저장, 관리하려면 Pillow 라이브러리 필요!!
+    
+    def __str__(self):
+        return self.name
+
