db88025d791a1d0a28942cb526636c48cde65b53
이지우 <20241257@sungshin.ac.kr>
2025-07-21 15:14:00 +0900

    백엔드2 관리자 권한 분리&상태 변경기능 추가

--- backend/meetagain/admin.py
+++ backend/meetagain/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-
-from django.contrib import admin
-from .models import LostItem
+from .models import LostItem, FoundItem
 
 admin.site.register(LostItem)
 
-#관리자 계정 아이디: admin 비번: 123456 이메일: test@example.com
+#관리자 계정 아이디: 12345678 비번: 123456 이메일: test@example.com
 # http://127.0.0.1:8000/admin/ 에서 로그인 후 LostItem 모델을 생성, 수정 가능.
+
+@admin.register(FoundItem)
+class FoundItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','status', 'found_location', 'found_date')
+    list_editable = ('status',)
+    list_filter = ('status', 'found_date')
+    search_fields = ('name', 'found_location')
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,41 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, student_id, email, password=None, **extra_fields):
+        if not student_id:
+            raise ValueError('The Student ID must be set')
+        email = self.normalize_email(email)
+        user = self.model(student_id=student_id, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, student_id, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(student_id, email, password, **extra_fields)
+
+
 class User(AbstractUser):
-    username = None #username은 항상 django에 기본적으로 포함. 학번으로 대체할 경우 username을 명시적으로 없애야함.
-    student_id = models.CharField(max_length=20, unique=True)  # 학번
+    username = None
+    student_id = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'student_id'  # 로그인 시 ID로 사용할 필드
+    USERNAME_FIELD = 'student_id'
     REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.student_id
