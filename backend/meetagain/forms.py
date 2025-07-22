d902b69c68af79e4d6240a12203ea00cf9e3a300
Jihyun <jessica092366@naver.com>
2025-07-22 20:01:42 +0900

    필수 입력 처리 및 미래 날짜 제한 기능 추가

--- backend/meetagain/forms.py
+++ backend/meetagain/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from .models import LostItem
-from .models import FoundItem
+from .models import LostItem, FoundItem
+from django.core.exceptions import ValidationError
+from datetime import date 
 
 class LostItemForm(forms.ModelForm):
     class Meta:
@@ -15,6 +16,29 @@ class LostItemForm(forms.ModelForm):
             'is_claimed',
             'image'
         ]
+    
+    def clean(self): #모든 필드가 입력되었는지 확인해주는 메서드
+        cleaned_data = super().clean()
+        required_fields = [
+            'name',
+            'description',
+            'category',
+            'lost_contact',
+            'lost_location',
+            'lost_date',
+            'image',
+        ] 
+
+        for field in required_fields:
+            if not cleaned_data.get(field):
+                self.add_error(field, f"{field} 항목은 필수입니다.") 
+
+        if 'is_claimed' not in cleaned_data: #BooleanField 별도 처리
+            self.add_error('is_claimed', "처리 여부를 선택해주세요.")
+
+        lost_date = cleaned_data.get('lost_date') #미래 날짜 입력 제한
+        if lost_date and lost_date > date.today():
+            self.add_error('lost_date', "미래 날짜는 입력할 수 없습니다.")
 
 class FoundItemForm(forms.ModelForm):
     class Meta:
@@ -30,3 +54,26 @@ class FoundItemForm(forms.ModelForm):
             'lat',
             'lng'
         ]
+    
+    def clean(self): #모든 필드가 입력되었는지 확인해주는 메서드
+        cleaned_data = super().clean()
+        required_fields = [
+            'name',
+            'description',
+            'category',
+            'found_location',
+            'image',
+            'lat',
+            'lng'
+        ]
+
+        for field in required_fields:
+            if not cleaned_data.get(field):
+                self.add_error(field, f"{field} 항목은 필수입니다.")
+
+        if 'is_returned' not in cleaned_data:
+            self.add_error('is_returned', "처리 여부를 선택해주세요.")
+        
+        found_date = cleaned_data.get('found_date')
+        if found_date and found_date > date.today():
+            self.add_error('found_date', "미래 날짜는 입력할 수 없습니다.")
